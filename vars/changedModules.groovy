a36b6450e0b6a177dd8cb3f8503aa635f5fbc6c4
j3r0lin <j3r0lin@icloud.com>
2017-08-27 16:51:55 +0800

    Add gitCommit step

--- vars/changedModules.groovy
+++ vars/changedModules.groovy
@@ -1,16 +1,13 @@
-def call(String ver1 = '', String ver2 = '', String prefix = '', String exclude = '') {
-    echo "Fetching change sets between $ver1, $ver2, prefix: $prefix, exclude: $exclude"
-    paths = changeSets(ver1, ver2)
+def call(String commit = '', String path = '', String exclude = '') {
+    echo "Fetching change sets from $commit, path: $path, exclude: $exclude"
+    paths = changeSets commit, path
 
     modules = []
     for (int i = 0; i < paths.size(); i++) {
-        def path = paths[i]
-        if (prefix == '' || path.startsWith(prefix)) {
-            path = path - prefix
-            if (exclude == '' || !path.startsWith(exclude)) {
-                if (path.split('/').size() > 1) {
-                    modules.add path.split('/')[0]
-                }
+        path = paths[i]
+        if (exclude == '' || !path.startsWith(exclude)) {
+            if (path.split('/').size() > 1) {
+                modules.add path.split('/')[0]
             }
         }
     }
@@ -18,8 +15,8 @@ def call(String ver1 = '', String ver2 = '', String prefix = '', String exclude 
 
 }
 
-def changeSets(String ver1, String ver2) {
-    def multiline = sh returnStdout: true, script: "git diff --name-only $ver1 $ver2"
+def changeSets(String commit, String path = '') {
+    def multiline = sh returnStdout: true, script: "git diff --name-only $commit -- $path"
     echo "Changed files:\n$multiline"
     multiline.readLines()
 }
