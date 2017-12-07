f5d972bba40ba72abf3e76640b1714eac97e0d51
j3r0lin <j3r0lin@icloud.com>
2017-12-08 00:35:44 +0800

    Add step to build docker image

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -7,7 +7,7 @@ def call(String type = 'java', List<String> services = [], String project = 'ele
             sh "cp Dockerfile ${name}/build/libs/"
             app = docker.build("${project}/${name}", "--no-cache --build-arg app=${name} ${name}/build/libs")
         } else if (type == 'erlang') {
-            app = docker.build("elements/${name}", "--no-cache _build/prod/rel/${name}")
+            app = docker.build("${project}/${name}", "--no-cache _build/prod/rel/${name}")
         }
 
         docker.withRegistry('https://registry.cn-hangzhou.aliyuncs.com', 'han-aliyun-registry') {
