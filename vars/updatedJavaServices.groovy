9cdae995c0dc906ad134a5453781267eff58d10b
j3r0lin <j3r0lin@icloud.com>
2017-12-08 01:15:06 +0800

    build all java services if common updated

--- vars/updatedJavaServices.groovy
+++ vars/updatedJavaServices.groovy
@@ -1,8 +1,11 @@
 def call(gitPreCommit = '', services = []) {
-    def dirs = changedModules(gitPreCommit).intersect(services)
+    def dirs = changedModules(gitPreCommit)
     if (dirs.contains('common') || params.buildJava || env.BRANCH_NAME ==~ /(master)|(release)|(PR).*/) {
         dirs = services
+    } else {
+        dirs = dirs.intersect(services)
     }
+
     echo "java services to build: ${dirs.join(',')}"
     return dirs
 }
