678b8b11c415b81b09613488d983cebe31dd95c6
j3r0lin <j3r0lin@icloud.com>
2017-12-12 21:51:41 +0800

    don't build all java services if common changed. user params.buildJava manually

--- vars/updatedJavaServices.groovy
+++ vars/updatedJavaServices.groovy
@@ -1,6 +1,6 @@
 def call(gitPreCommit = '', services = []) {
     def dirs = changedModules(gitPreCommit)
-    if (dirs.contains('common') || params.buildJava || env.BRANCH_NAME ==~ /(master)|(release)|(PR).*/) {
+    if (params.buildJava || env.BRANCH_NAME ==~ /(master)|(release)|(PR).*/) {
         dirs = services
     } else {
         dirs = dirs.intersect(services)
