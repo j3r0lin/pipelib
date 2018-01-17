a6ab1267228036c770abaf5c87d5fc4cd0580152
j3r0lin <chenjian@inhand.com.cn>
2018-01-18 01:14:54 +0800

    Update updatedJavaServices.groovy

--- vars/updatedJavaServices.groovy
+++ vars/updatedJavaServices.groovy
@@ -1,6 +1,6 @@
 def call(gitPreCommit = '', services = []) {
     def dirs = changedModules(gitPreCommit)
-    if (params.buildJava || env.BRANCH_NAME ==~ /(master)|(release)|v|(PR).*/) {
+    if (params.buildJava || env.BRANCH_NAME ==~ /^(master|release|v|PR).*/ {
         dirs = services
     } else {
         dirs = dirs.intersect(services)
